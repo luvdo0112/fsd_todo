2f9657018de33c93dcc6a5f018f537c119101eae
chldmswnl <chldmswnl12@gmail.com>
2024-03-03 00:49:13 +0900

    style: TodoCard 컴포넌트 style 변경

--- src/widgets/TodoCard/TodoCard.styled.ts
+++ src/widgets/TodoCard/TodoCard.styled.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  padding: 1.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 2rem;
 
   border-radius: 1rem;
-  background-color: beige;
 `;
 
 export const TodoInfoSection = styled.div`
@@ -14,12 +17,22 @@ export const TodoInfoSection = styled.div`
   row-gap: 1rem;
 `;
 
-export const TodoTitle = styled.span`
-  font-size: 1.4rem;
+export const TodoTitle = styled.span<{ $checked: boolean }>`
+  font-size: 1.8rem;
   font-weight: 700;
+  ${({ $checked }) =>
+    $checked &&
+    `
+    text-decoration: line-through;;
+    color: #808080;
+  `}
 `;
 
-export const TodoTime = styled.span`
-  font-size: 1.2rem;
-  color: #b9beca;
+export const RadioBtn = styled.button<{ $checked: boolean }>`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50rem;
+  border: ${({ $checked }) =>
+    $checked ? "1px solid transparent" : "1px solid #808080"};
+  background-color: ${({ $checked }) => ($checked ? "#E1E0F2" : "#fff")};
 `;
